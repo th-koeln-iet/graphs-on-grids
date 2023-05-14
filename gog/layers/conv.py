d33875373ef84b972b9ecfbec22b8085acb4908a
akletini <allen.k@freenet.de>
2023-05-14 19:24:48 +0200

    Implement Graph Convolution Layer

--- gog/layers/conv.py
+++ gog/layers/conv.py
@@ -3,29 +3,31 @@ import tensorflow as tf
 from tensorflow import keras
 
 
-class GNN(keras.layers.Layer):
+class GCN(keras.layers.Layer):
+
     def __init__(self, adjacency_matrix: np.ndarray, embedding_size, use_bias=True, activation=None,
-                 aggregation_method="sum", weight_initializer="glorot_uniform", bias_initializer="zeros"):
-        super(GNN, self).__init__()
+                 weight_initializer="glorot_uniform", bias_initializer="zeros"):
+        super(GCN, self).__init__()
         self.embedding_size = int(embedding_size) if not isinstance(embedding_size, int) else embedding_size
         if embedding_size <= 0:
             raise ValueError(
                 f"Received invalid embedding_size, expected positive integer. Received embedding_size={embedding_size}")
-        if aggregation_method != "sum" and aggregation_method != "mean":
-            raise ValueError(
-                f"Received invalid aggregation method={aggregation_method}. Valid options are 'sum' or 'mean'.")
+
         self.adjacency_matrix = adjacency_matrix
         self.use_bias = use_bias
         self.activation = keras.activations.get(activation)
-        self.aggregation_method = aggregation_method
         self.weight_initializer = keras.initializers.get(weight_initializer)
         self.bias_initializer = keras.initializers.get(bias_initializer)
 
         # Adjacency matrix with self loops
         self._A_tilde = tf.math.add(self.adjacency_matrix, tf.eye(self.adjacency_matrix.shape[0]))
 
-        # Degree vector of adjacency matrix
-        self._D = tf.reduce_sum(self._A_tilde, axis=1)
+        # Degree matrix of adjacency matrix
+        D = tf.zeros_like(self._A_tilde)
+        D = tf.linalg.set_diag(D, tf.reduce_sum(self._A_tilde, axis=1))
+
+        # Inverse of square root of degree matrix
+        self._D_mod = tf.linalg.inv(tf.linalg.sqrtm(D))
 
     def build(self, input_shape):
         self.W = self.add_weight(
@@ -34,10 +36,8 @@ class GNN(keras.layers.Layer):
         self.b = self.add_weight(shape=(self.embedding_size,), initializer=self.bias_initializer, trainable=True)
 
     def call(self, inputs, *args, **kwargs):
-        masked_feature_matrix = tf.matmul(self._A_tilde, inputs)
-
-        if self.aggregation_method == "mean":
-            masked_feature_matrix = tf.divide(masked_feature_matrix, self._D[:, None])
+        A_hat = tf.matmul(tf.matmul(self._D_mod, self._A_tilde), self._D_mod)
+        masked_feature_matrix = tf.matmul(A_hat, inputs)
 
         output = tf.matmul(masked_feature_matrix, self.W)
 
