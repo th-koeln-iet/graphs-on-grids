Introduce batch averaging on attention-scores and minor fixes

graphs_on_grids/layers/attention.py
@@ -101,7 +101,7 @@ class GraphAttention(GraphLayer):
         # Normalize attention scores
         attention_scores = tf.math.exp(tf.clip_by_value(attention_scores, -2, 2))
         attention_scores_sum = tf.math.unsorted_segment_sum(
-            data=attention_scores[-1, :],
+            data=tf.reduce_mean(attention_scores, axis=0),
             segment_ids=self.edges[:, 0],
             num_segments=tf.reduce_max(self.edges[:, 0]) + 1,
         )
@@ -111,10 +111,10 @@ class GraphAttention(GraphLayer):
         attention_scores_norm = attention_scores / attention_scores_sum
 
         # apply attention scores and aggregate
-        # last attention score can be taken since the attention values are equal for the whole batch
+        # attention scores are averaged for the whole batch
         # write normalized attention scores to position where adjacency_matrix equals one
         weighted_adj = tf.tensor_scatter_nd_update(
-            self._A_tilde, self.edges, attention_scores_norm[-1]
+            self._A_tilde, self.edges, tf.reduce_mean(attention_scores_norm, axis=0)
         )
         output = tf.matmul(weighted_adj, node_states_transformed)
 

graphs_on_grids/layers/conv.py
@@ -63,6 +63,7 @@ class GraphConvolution(GraphLayer):
 
         # Inverse of square root of degree matrix
         self._D_mod = tf.linalg.inv(tf.linalg.sqrtm(D))
+        self._A_hat = tf.matmul(tf.matmul(self._D_mod, self._A_tilde), self._D_mod)
 
     def build(self, input_shape):
         if len(input_shape) == 2:
@@ -97,11 +98,11 @@ class GraphConvolution(GraphLayer):
                 self.adjacency_matrix, self.edges, edge_weights_avg
             )
             self._A_tilde = weighted_adj_matrix
+            self._A_hat = tf.matmul(tf.matmul(self._D_mod, self._A_tilde), self._D_mod)
         else:
             node_features = inputs
 
-        A_hat = tf.matmul(tf.matmul(self._D_mod, self._A_tilde), self._D_mod)
-        masked_feature_matrix = tf.matmul(A_hat, node_features)
+        masked_feature_matrix = tf.matmul(self._A_hat, node_features)
 
         output = self.node_feature_MLP(masked_feature_matrix)
 

graphs_on_grids/layers/graph_layer.py
@@ -210,7 +210,7 @@ class GraphLayer(keras.layers.Layer):
         )
         # contains concatenation of neighbor node pair features and corresponding edge features
         # if a pair contains a self-loop, the edge feature vector is zeroed.
-        # Shape (batch_size, |E| + |V|, 2 * |X_v| + |E|)
+        # Shape (batch_size, |E| + |V|, |X_v| + |X_e|)
         node_states_expanded = tf.concat([node_states_expanded, edge_features], axis=2)
         return node_states_expanded
 